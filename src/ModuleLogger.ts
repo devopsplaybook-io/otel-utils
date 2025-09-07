eb3b267a3855dd430c76c8a3744f37f2a4e8c121
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2025-09-07 20:57:10 +0800

    wip

--- src/ModuleLogger.ts
+++ src/ModuleLogger.ts
@@ -10,6 +10,7 @@ export class ModuleLogger {
     this.standardLogger = standardLogger;
   }
 
+<<<<<<< Updated upstream
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public info(message: Error | string | any): void {
     this.display("info", message, SeverityNumber.WARN);
@@ -21,10 +22,23 @@ export class ModuleLogger {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public error(message: Error | string | any): void {
     this.display("error", message, SeverityNumber.ERROR);
+=======
+  public info(message: string, context?: Span): void {
+    this.display("info", message, SeverityNumber.INFO, null, context);
+  }
+
+  public warn(message: string, context?: Span): void {
+    this.display("warn", message, SeverityNumber.WARN, null, context);
+  }
+
+  public error(message: string, error?: Error, context?: Span): void {
+    this.display("error", message, SeverityNumber.ERROR, error, context);
+>>>>>>> Stashed changes
   }
 
   private display(
     level: string,
+<<<<<<< Updated upstream
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     message: any,
     severityNumber = SeverityNumber.INFO
@@ -40,6 +54,24 @@ export class ModuleLogger {
     } else if (typeof message === "object") {
       // eslint:disable-next-line:no-console
       console.log(`${level} [${this.module}] ${JSON.stringify(message)}`);
+=======
+    message: string,
+    severityNumber = SeverityNumber.INFO,
+    error?: Error | null,
+    context?: Span
+  ): void {
+    let formattedMessage = message;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const attributes: Record<string, any> = { "log.type": "custom" };
+
+    formattedMessage = message;
+    if (error) {
+      attributes["exception.type"] = error.name;
+      attributes["exception.message"] = error.message;
+      attributes["exception.stacktrace"] = error.stack;
+      formattedMessage += "\n" + error.stack;
+>>>>>>> Stashed changes
     }
     if (!this.standardLogger?.getLogger()) {
       return;
